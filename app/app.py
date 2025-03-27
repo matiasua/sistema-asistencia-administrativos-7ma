be1d8689eac65d068d8c067a3bcaabec5bd1c5d9
matiasua <matiasncp7@gmail.com>
2025-03-27 15:16:21 -0300

    se agrega puerto

--- app/app.py
+++ app/app.py
@@ -335,5 +335,5 @@ def get_filtered_registros():
     return query.all()
 
 if __name__ == '__main__':
-    # Ejecutar la aplicación en modo debug para desarrollo
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
